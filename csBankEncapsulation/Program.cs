3803748d0bb8e1b3da8e9b9e353943037d11838b
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-06-13 13:59:58 -0500

    initial commit

---
+++ csBankEncapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csBankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new BankAccount();
+            b.Deposit(100.00);
+            Console.WriteLine(b.GetBalance());
+        }
+    }
+}
